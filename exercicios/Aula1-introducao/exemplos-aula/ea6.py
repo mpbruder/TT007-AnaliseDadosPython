4b04169eff60622ea0b018d7e729f82055de3161
Matheus Bruder <matheuspercario@gmail.com>
2020-11-10 09:52:06 -0300

    Finalizando exemplos - aula 01

--- exercicios/Aula1-introducao/exemplos-aula/ea6.py
+++
@@ -1,8 +0,0 @@
-# TT007 - Introdução à Ciência de Dados
-
-# Criado por: Matheus Percário Bruder
-
-# Exemplo
-"""
-Escreva um programa que converta um número binário (base 2) em decimal (base 10). Seu programa deve começar lendo o número binário do usuário como uma string. Então ele deve calcular o número decimal equivalente processando cada dígito no número binário. Finalmente, seu programa deve exibir o número decimal equivalente.
-"""
